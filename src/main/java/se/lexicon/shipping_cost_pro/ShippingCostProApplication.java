2010244cff129de2436fb036881538b486be00a2
olaulvas <ola.ulvaas@gmail.com>
2021-04-14 21:45:19 +0200

    Add Bootstrap Dependencies. Build Project Structure with BoxForm.html, BoxList.html, Controller, Entity, Exception, Repository and Service Directions.

---
+++ src/main/java/se/lexicon/shipping_cost_pro/ShippingCostProApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingCostProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingCostProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/shipping_cost_pro/ShippingCostProApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCostProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
